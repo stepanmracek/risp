8efe110bc92271a53e5c5850a3474894f9951409
Štěpán Mráček <stepan.mracek@gmail.com>
2025-12-01 23:08:47 +0100

    AOC2025 day1

---
+++ aoc2025/day1/part1.scm
@@ -0,0 +1,19 @@
+(begin
+    (define pos 50)
+    (define inputs (split-string (read-file "aoc2025/day1/input")))
+    (define inputs (map (lambda (s) (list (substring s 0 1) (parse-int (substring s 1 -1)))) inputs))
+
+    (define (dial direction amount)
+        (begin
+            (if (= direction "R")
+                (set! pos (mod (+ pos amount) 100))
+                (set! pos (mod (- pos amount) 100))
+            )
+            pos
+        )
+    )
+
+    (define positions (map (lambda (input) (apply dial input)) inputs))
+    (define zero-count (apply + (map (lambda (input) (if (= input 0) 1 0)) positions)))
+    (display zero-count)
+)
