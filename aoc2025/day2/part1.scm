a7508e478be0e5e8d9b8c4a6ac25749c43a7471a
Štěpán Mráček <stepan.mracek@gmail.com>
2025-12-03 23:13:36 +0100

    AoC2025, day2, part1

---
+++ aoc2025/day2/part1.scm
@@ -0,0 +1,43 @@
+(begin
+    (define pos 50)
+    (define inputs (split-string-with (read-file "aoc2025/day2/input") ","))
+    (define (parse-input s)
+        (begin
+            (define strings (split-string-with s "-"))
+            (map string->int strings)
+        )
+    )
+    (define inputs (map parse-input inputs))
+
+    (define invalid-ids 0)
+    (define (count-invalid-ids start stop)
+        (begin
+            (define generator (make-generator start 1))
+
+            (define (check-value generator)
+                (begin
+                    (define val (generator))
+                    (if (<= val stop)
+                        (begin
+                            (define s (->string val))
+                            (define l (length s))
+                            (define left (substring s 0 (/ l 2)))
+                            (define right (substring s (/ l 2) l))
+                            (if (= left right)
+                                (set! invalid-ids (+ invalid-ids val))
+                                #f
+                            )
+                            (check-value generator)
+                        )
+                        #f
+                    )
+                )
+            )
+
+            (check-value generator)
+        )
+    )
+
+    (map (lambda (input) (apply count-invalid-ids input)) inputs)
+    (display invalid-ids)
+)
